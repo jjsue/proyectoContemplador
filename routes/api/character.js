d1ed51f51d61c04a099a9715c87aa0ac70514b40
Jjsue <jjsue_95@hotmail.es>
2020-07-03 13:52:45 +0200

    Filtros de los personajes finalizados. sort: recent | old name: race: class: levelMax: levelMin:

--- routes/api/character.js
+++ routes/api/character.js
@@ -5,21 +5,27 @@ const Pnj = require('./../../models/pnj');
 router.get('/', async (req, res, next) => {
     try {
         const filtro = {};
-        console.log(req.query.sort);
         let orderBy = req.query.sort === 'old' ? 1 : -1 //Ordenar por mas reciente o mas antiguo, por defecto ordena por reciente
-        var sort = {creationDate: orderBy};
+        var sort = { creationDate: orderBy };
         var skip = 0;
         var limit = 100;
-        const fields = 'name clase nivel raza, creationDate';
-        //filtro.isPublic = true; //Nos aseguramos que los personajes que listamos son los publicos.
+        const fields = 'name clase nivel raza';
+        filtro.isPublic = true; //Nos aseguramos que los personajes que listamos son los publicos.
         if (req.query.name !== undefined) {
             filtro.name = new RegExp(req.query.name, "gi");
         }
         if (req.query.race !== undefined) {
-            filtro.raza = req.query.sell;
+            filtro.raza = new RegExp(req.query.race, "gi");
         }
-        if (req.query.orderby !== undefined && (req.query.orderby === 'name' || req.query.orderby === 'price')) {
-            sort = req.query.orderby;
+        if (req.query.class !== undefined) {
+            filtro.clase = new RegExp(req.query.class, "gi");
+        }
+        if (req.query.levelMax !== undefined && req.query.levelMin !== undefined) {
+            if (isNaN(parseInt(req.query.levelMin)) || isNaN(parseInt(req.query.levelMax))) {
+                filtro.nivel = { $gte: 1, $lte: 20 }
+            } else {
+                filtro.nivel = { $gte: parseInt(req.query.levelMin), $lte: parseInt(req.query.levelMax) } //Si ambos son numeros creamos de este modo el filtro
+            }
         }
         if (req.query.skip !== undefined) {
             skip = parseInt(req.query.skip);
