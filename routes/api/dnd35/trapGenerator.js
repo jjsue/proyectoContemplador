7769b45259c950a823dc3e39e209b08e31565814
Jjsue <jjsue_95@hotmail.es>
2020-09-14 11:58:57 +0200

    Primeras pinceladas del trap generator

--- routes/api/dnd35/trapGenerator.js
+++ routes/api/dnd35/trapGenerator.js
@@ -2,12 +2,20 @@
 var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
+const trapDisparador = require("./../../../lib/dnd35/operaciones/trapDisparador");
+const trapRearme = require("./../../../lib/dnd35/operaciones/trapRearme");
+const trapDesactivacion = require("./../../../lib/dnd35/operaciones/trapDesactivacion");
 //Validaciones custom
 router.post('/',
     async (req, res, next) => {
         try {
-            //Por ahora van a ser trampas mecánicas, ya que las trampas mágicas requeririan de conjuros.
-            res.send("Hi");
+            const newTrap = {};
+            newTrap.disparador = trapDisparador();
+            newTrap.rearme = trapRearme()
+            newTrap.desactivacion = trapDesactivacion();
+            
+            //Respondemos
+            res.json(newTrap);
 
         }
         catch (err) {
