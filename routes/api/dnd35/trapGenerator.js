0349dad9e2fc576525cd441b1df92410b294cd89
Jjsue <jjsue_95@hotmail.es>
2020-09-23 12:31:01 +0200

    Versión muy básica de las trampas finalizada

--- routes/api/dnd35/trapGenerator.js
+++ routes/api/dnd35/trapGenerator.js
@@ -5,6 +5,8 @@ const { check, validationResult } = require('express-validator');
 const trapDisparador = require("./../../../lib/dnd35/operaciones/trapDisparador");
 const trapRearme = require("./../../../lib/dnd35/operaciones/trapRearme");
 const trapDesactivacion = require("./../../../lib/dnd35/operaciones/trapDesactivacion");
+const trapEfecto = require("./../../../lib/dnd35/operaciones/trapEfecto");
+const trapDano = require("./../../../lib/dnd35/operaciones/trapDano");
 //Validaciones custom
 router.post('/',
     async (req, res, next) => {
@@ -13,7 +15,8 @@ router.post('/',
             newTrap.disparador = trapDisparador();
             newTrap.rearme = trapRearme()
             newTrap.desactivacion = trapDesactivacion();
-            
+            newTrap.efecto = trapEfecto();
+            newTrap.dano = trapDano(newTrap.efecto[0]);
             //Respondemos
             res.json(newTrap);
 
